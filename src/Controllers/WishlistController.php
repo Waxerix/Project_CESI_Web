751a11ef18a67d706fef14f1738d607f5e047467
mathis <mathisboulenger@gmail.com>
2026-03-31 16:44:56 +0200

    wishlist sans page

---
+++ src/Controllers/WishlistController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controllers;
+use App\Core\Controller;    
+use App\Models\WishlistModel;
+
+class WishlistController extends Controller {
+
+    public function __construct($twig, $pdo) {
+        // On utilise les propriétés héritées de Controller.php
+        $this->twig = $twig;
+        $this->pdo = $pdo;
+        // On initialise le modèle
+        $this->model = new WishlistModel($this->pdo);
+    }
+
+    public function index() {
+        $id_user = 1; 
+        // Vérifie bien que la méthode s'appelle getAllByUserId dans le modèle
+        $offres = $this->model->getAllByUserId($id_user);
+
+        echo $this->twig->render('wishlist.html.twig', [
+            'offres' => $offres,
+            'page_title' => 'Ma Wish-list'
+        ]);
+    }
+
+    public function add($id_offer) {
+        $id_user = 1; 
+        if ($id_offer) {
+            $this->model->add($id_user, $id_offer);
+        }
+        header('Location: /wishlist');
+        exit();
+    }
+
+    public function delete($id_offer) {
+        $id_user = 1; 
+        if ($id_offer) {
+            $this->model->remove($id_user, $id_offer);
+        }
+        header('Location: /wishlist');
+        exit();
+    }
+}
