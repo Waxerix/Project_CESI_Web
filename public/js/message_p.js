5d930835bb4466a88117e4b7444ef1c1fffa2cf7
mathis <mathisboulenger@gmail.com>
2026-04-01 14:39:07 +0200

    postuler terminer

--- public/js/message_p.js
+++ public/js/message_p.js
@@ -1,7 +1,1 @@
-const params = new URLSearchParams(window.location.search);
-
-if (window.location.hash === '#success') {
-    alert('Votre candidature a bien été envoyée !');
-    // Nettoie l'URL
-    window.history.replaceState({}, '', '/');
-}
+alert('Votre candidature a bien été envoyée !');
